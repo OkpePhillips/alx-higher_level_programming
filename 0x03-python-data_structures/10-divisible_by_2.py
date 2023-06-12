a570a0dfc7b94db82c03e22b00980998347ae172
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-06-12 12:21:40 -0700

    fixed indexing of iterator in the list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
     for i in new_list:
         if i % 2 == 0:
-            new_list[i] = True
+            new_list[new_list.index(i)] = True
         else:
-            new_list[i] = False
+            new_list[new_list.index(i)] = False
     return new_list
