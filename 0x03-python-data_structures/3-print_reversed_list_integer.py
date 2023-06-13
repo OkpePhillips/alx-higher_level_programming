0810e66327ce21ef23497534d2ab196b3dc71e62
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-06-13 04:27:00 -0700

    Fixed an issue with modifying the list directly

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        my_list = []
+    else:
+        my_list = list(my_list)
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
