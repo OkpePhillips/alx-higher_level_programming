309d1fe5a3f9168126df9f1363903f40824e034f
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-06-14 13:21:21 -0700

    Reworked the code for better performance

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,6 @@
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for key, value in a_dictionary.items():
-        new_dictionary[key] = value * 2
+        new_value = value * 2
+        new_dictionary[key] = new_value
     return new_dictionary
