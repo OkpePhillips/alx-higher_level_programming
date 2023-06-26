fad977940edcf60f888048e5c819c7099af2b8d2
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-06-26 15:24:44 -0700

    Python Exception handling task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except TypeError:
+            break
+        else:
+            printed += 1
+    print()
+    return printed
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,2 @@
+#0x05-python-exceptions
+This folder contains various tasks aimed at learning more about python errors and how to handle them.
