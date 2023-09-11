fe1f8009cc422f7ecd5a9c7feccb5034a0e7d6c7
Godwin Okpe <okpegodwin18@yahoo.com>
2023-09-11 14:08:04 -0700

    Added more tasks, but could not correct semistandard errors since I am pushing via my phone

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Script to print the second biggest integer in the argument list
+let argumentList = process.argv.slice(2);
+if (argumentList.length === 0 || argumentList.length === 1) {
+  console.log(0);
+} else {
+  argumentList = argumentList.map(Number);
+  argumentList.sort(function (a, b) {return b - a});
+  const secondBiggest = argumentList[1];
+  console.log(secondBiggest);
+}
