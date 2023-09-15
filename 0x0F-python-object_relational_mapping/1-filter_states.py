14c1f74683332e058f0224c6adddca3b5c459a6b
Godwin Okpe <okpegodwin18@yahoo.com>
2023-09-15 08:17:18 -0700

    fixing requirement issue in task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY\
                    states.id ASC")
     states = cursor.fetchall()
 
