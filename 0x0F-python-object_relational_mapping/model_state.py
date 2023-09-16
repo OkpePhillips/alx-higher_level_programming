db22106e52b81c6aa1563f90549b05dd52e67fe4
Godwin Okpe <okpegodwin18@yahoo.com>
2023-09-16 04:32:34 -0700

    Added more tasks

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A python file that contains the class definition of a
+State and an instance Base = declarative_base()
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+import sys
+from sqlalchemy.orm import relationship
+from model_city import City
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                       sys.argv[1], sys.argv[2], sys.argv[3]))
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
