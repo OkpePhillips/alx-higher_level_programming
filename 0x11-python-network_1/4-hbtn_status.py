381de519b8a1b9bdde759bfa7fe846f5ccae80e7
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-10-01 06:23:49 -0700

    Trying a fix for status task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
     t = r.text
     print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
